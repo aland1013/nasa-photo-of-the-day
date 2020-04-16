d501b47f1434022306a8008854e8a7595329355f
aland1013 <adam.land@gmail.com>
2020-04-16 14:27:22 -0400

    restyled Picture.js

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,15 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const HR = styled.hr`
+  width: 30%;
+  border-style: solid;
+`;
+
+const H2 = styled.h2`
+  font-size: 40px;
+  font-family: 'Open Sans', sans-serif;
+  text-align: center;
+`;
+
+const NasaImage = styled.img`
+  display: block;
+  width: 70%;
+  margin: 20px auto;
+  border-radius: 3px;
+`;
+
+const ContentContainer = styled.div`
+  box-sizing: border-box;
+  border: 1px solid lightgray;
+  border-radius: 3px;
+  padding: 20px;
+  margin: 20px 0;
+
+`;
+
+const P = styled.p`
+font-size: 20px;
+font-family: 'Open Sans', sans-serif;
+`;
 
 const Picture = (props) => {
   const { title, url, explanation } = props;
 
   return (
     <>
-      <div className="ui horizontal divider"><h3>{title}</h3></div>
-      <img className="ui centered huge rounded image" src={url} alt="" />
-      <div className="ui segment">
-        <p>{explanation}</p>
-      </div>
+      <FlexContainer>
+        <HR/><H2>{title}</H2><HR/>
+      </FlexContainer>
+      
+      <NasaImage src={url} alt="" />
+      <ContentContainer>
+        <P>{explanation}</P>
+      </ContentContainer>
     </>
   );
 }
